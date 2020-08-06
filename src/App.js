876e8e090bb0d5a11474fbb6c18f53ace8b6c081
lee <lee2py@gmail.com>
2020-08-06 22:10:16 +0800

    add node-sass and some test scss module file.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import testStyle from './test.module.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className={testStyle.redBackground}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
