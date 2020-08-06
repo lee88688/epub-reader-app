58181ae4e7d09074fdbb6b553e3c93d5dee5955a
lisi <lisi@cmiot.chinamobile.com>
2020-08-06 17:27:44 +0800

    remove idea folder, add dependances

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'fontsource-roboto';
+import CssBaseline from "@material-ui/core/CssBaseline";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
